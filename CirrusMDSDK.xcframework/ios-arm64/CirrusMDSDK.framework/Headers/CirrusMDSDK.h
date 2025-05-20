da50526ac765d97590a3a841618905bee7eefa5e
Tom Klucher <thomas-klucher@cirrusmd.com>
2025-05-20 15:43:57 -0700

    CirrusMD SDK Package dot Swift

---
+++ CirrusMDSDK.xcframework/ios-arm64/CirrusMDSDK.framework/Headers/CirrusMDSDK.h
@@ -0,0 +1,20 @@
+//
+//  CirrusMDSDK.h
+//  CirrusMDSDK
+//
+//  Created by Sean Dougherty on 10/10/17.
+//  Copyright © 2017 CirrusMD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+//! Project version number for CirrusMDSDK.
+FOUNDATION_EXPORT double CirrusMDSDKVersionNumber;
+
+//! Project version string for CirrusMDSDK.
+FOUNDATION_EXPORT const unsigned char CirrusMDSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CirrusMDSDK/PublicHeader.h>
+
+
---
+++ Package.swift
@@ -0,0 +1,38 @@
+// swift-tools-version:5.5
+import PackageDescription
+let package = Package(
+    name: "CirrusMDSDKSPM",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "CirrusMDSDKSPM",
+            targets: [
+                "CirrusMDSDKTargets"
+            ]
+        )
+    ],
+    dependencies: [
+        .package(name: "Kingfisher",
+                 url: "https://github.com/onevcat/Kingfisher",
+                 .exact("8.1.3")),
+        .package(name: "AmazonChimeSDK",
+                 url: "https://github.com/aws/amazon-chime-sdk-ios-spm",
+                 .exact("0.26.2"))
+    ],
+    targets: [
+        .binaryTarget(
+            name: "CirrusMDSDK",
+            path: "CirrusMDSDK.xcframework"
+        ),
+        .target(name: "CirrusMDSDKTargets",
+                dependencies: [
+                    .target(name: "CirrusMDSDK"),
+                    .product(name: "Kingfisher", package: "Kingfisher"),
+                    .product(name: "AmazonChimeSDK", package: "AmazonChimeSDK")
+                ],
+                path: "Sources"
+        )
+    ]
+)
